775fd8a4a94dca36d74b7cea3827cee209baae63
ParadigmMango <jacobjhartt@gmail.com>
2025-10-28 22:01:58 -0600

    added count limit algorithm

--- benches.js
+++ benches.js
@@ -1,8 +1,47 @@
-const gNumLimit = 1000000;
-
-function matchRatios(a, b, min, max, threshold) {
+function matchRatiosCountLimits(a, b, min, max, threshold) {
   var aCount = min;
   var bCount = min;
+  var aSum = a * aCount;
+  var bSum = b * bCount;
+  var diff = Math.abs(aSum - bSum);
+  var minDiff;
+  const closestRatios = [];
+  if (diff < threshold) {
+    minDiff = diff;
+    closestRatios.push([aCount, bCount, aSum, bSum, diff, "Closest Yet"]);
+  } else {
+    minDiff = threshold;
+  }
+
+  while (aCount < max && bCount < max) {
+    // incriment sums
+    if (aSum < bSum) {
+      aSum += a;
+      aCount++;
+    } else {
+      bSum += b;
+      bCount++;
+    }
+
+    // Count new closest ratios
+    diff = Math.abs(aSum - bSum);
+    if (diff < threshold) {
+      if (diff < minDiff) {
+        minDiff = diff;
+        closestRatios.push([aCount, bCount, aSum, bSum, diff, "Closest Yet"]);
+      } else {
+        closestRatios.push([aCount, bCount, aSum, bSum, diff, "Not Closest Yet"]);
+      }
+    }
+  }
+
+  return closestRatios;
+}
+
+
+function matchRatiosSumLimits(a, b, min, max, threshold) {
+  var aCount = Math.floor(min / a);
+  var bCount = Math.floor(min / b);
   var aSum = a * aCount;
   var bSum = b * bCount;
   var diff = Math.abs(aSum - bSum);
@@ -40,13 +79,13 @@ function matchRatios(a, b, min, max, threshold) {
   return closestRatios;
 }
 
-// console.log("Hi from deno");
-// console.log(matchRatios(Math.PI, 1, 1, 1000, 0.1));
+console.log(matchRatiosCountLimits(Math.PI, 3, 1, 1000, 0.1));
+console.log(matchRatiosSumLimits(Math.PI, 3, 1, 1000, 0.1));
 
-Deno.bench({
-  name: "straightforward",
-  baseline: true,
-  fn: () => {
-    matchRatios(Math.PI, 1, 1, gNumLimit, 0.1);
-  }
-});
+// Deno.bench({
+  // name: "straightforward",
+  // baseline: true,
+  // fn: () => {
+    // matchRatios(Math.PI, 1, 1, gNumLimit, 0.1);
+  // }
+// });
